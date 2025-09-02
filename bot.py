1bc894705efa15ac64936559825fd253c2f5eb8e
htetnaung6224-lab <htetnaung6224@gmail.com>
2025-09-02 07:49:28 +0630

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import os
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-
-BOT_TOKEN = os.environ["7559095836:AAG50xgGTiAG3hGLYS8tYJtxxqoPqjHPXpA"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
@@ -13,15 +12,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [InlineKeyboardButton("Honor of Kings Token", url="https://t.me/yourshoplink")],
         [InlineKeyboardButton("BIGO Live Dia", url="https://t.me/yourshoplink")],
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("🎮 Game Top-up Menu:", reply_markup=reply_markup)
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    print("✅ Bot is running on Render...")
     app.run_polling()
 
 if __name__ == "__main__":
